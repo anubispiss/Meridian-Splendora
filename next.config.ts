b37bec7808ae8e33aa5b926880b46d52bc286fc7
anubispiss <swagatabanerjee02@gmail.com>
2025-02-04 02:15:31 +0530

    First Commit

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+//import type { NextConfig } from "next";
+
+//const nextConfig: NextConfig = {
+  /* config options here */
+//};
+
+//export default nextConfig;
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'],
+  },
+};
+
+export default nextConfig;
